fed98939a18cc9c93d4553defedba39a1d7c69f2
Giorgia <giorgia.giro@gmail.com>
2025-01-20 15:20:54 +0200

    fixed formatter

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import ButtonGradient from "./assets/svg/ButtonGradient"
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import ButtonGradient from "./assets/svg/ButtonGradient";
 
+const App = () => {
+    return (
+        <>
+            <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
+                <h1>sxhecw</h1>
+                <button className="mt-10" href="#login">
+                    Something
+                </button>
+            </div>
+            <ButtonGradient />
+        </>
+    );
+};
 
-const App=()=> {
-  return (
-    <>
-     <div className='pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden'>
-      <Button className="mt-10" href="#login">
-          Something
-      </Button>
-     </div>
-     <ButtonGradient/>
-    </>
-  )
-}
-
-export default App
+export default App;
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
+import ButtonSvg from "../assets/svg/ButtonSvg"
 
-const Button = () => {
-  return (
-    <div>Button</div>
-  )
-}
+const Button = ({ className, href, onClick, children, px, white }) => {
 
-export default Button
+    const classes = `button relative inline-flex items-center justify-center h-11 transition-colors hover:text-color-1 ${
+        px || "px-7"
+      } ${white ? "text-n-8" : "text-n-1"} ${className || ""}`;
+
+    const spanClasses = "relative z-10";
+
+    const renderButton = () => (
+        <button className={classes}>
+        <span>{children}    </span>
+        {ButtonSvg(white)}
+        </button>
+    );
+};
+
+export default Button;
