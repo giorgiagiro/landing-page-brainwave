1ab644a944b5a0bb7bde8e60a0b72fbbd5def7da
Giorgia <giorgia.giro@gmail.com>
2025-01-20 14:43:19 +0200

    first button component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import ButtonGradient from "./assets/svg/ButtonGradient"
 
 
-function App() {
+const App=()=> {
   return (
     <>
-     
+     <div className='pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden'>
+      <Button className="mt-10" href="#login">
+          Something
+      </Button>
+     </div>
+     <ButtonGradient/>
     </>
   )
 }
