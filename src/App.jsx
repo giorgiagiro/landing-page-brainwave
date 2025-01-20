b4c526826cf675347e5c28ac886db81b09b98e3f
Giorgia <giorgia.giro@gmail.com>
2025-01-20 17:07:10 +0200

    Section, setup hero

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import ButtonGradient from "./assets/svg/ButtonGradient";
 import Header from "./components/Header";
+import Hero from "./components/Hero";
 
 const App = () => {
     return (
         <>
             <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
                 <Header />
+                <Hero />
             </div>
             <ButtonGradient />
         </>
