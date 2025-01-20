85e6fa1af8d52f89dda4f24cf9cc6bc3a5edca40
Giorgia <giorgia.giro@gmail.com>
2025-01-20 17:17:21 +0200

    hero section update

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,8 @@
 import React from "react";
 import Section from "./Section";
+import Button from "./Button";
+
+import { curve } from "../assets";
 
 const Hero = () => {
     return (
@@ -11,11 +14,27 @@ const Hero = () => {
             id="hero"
         >
             <div className="container relative">
-                <div className="relative z-1 max-w-[62rem] mx-auto text-center mb-[4rem] md:mb-20 lg:mb-[6rem]">
+                <div className="relative z-1 max-w-[62rem] mx-auto text-center mb-[3.875rem] md:mb-20 lg:mb-[6.25rem]">
                     <h1 className="h1 mb-6">
-                        Explore the Possibilities of AI Chatting with BrainwaveCurve
-                        <span className="inline-block-relative">Brainwave</span>
+                        Explore the Possibilities of&nbsp;AI&nbsp;Chatting with {` `}
+                        <span className="inline-block-relative">
+                            Brainwave{" "}
+                            <img
+                                src={curve}
+                                className="absolute top-full left-0 w-full xl:-mt-2"
+                                width={624}
+                                height={28}
+                                alt="Curve"
+                            ></img>
+                        </span>
                     </h1>
+                    <p className="body-1 max-w-3xl mx-auto mb-6 text-n-2 lg:mb-8">
+                        Unleash the power of AI within Brainwave. Upgrade your productivity with
+                        Brainwave, the open AI chat app.
+                    </p>
+                    <Button href="/pricing" white>
+                        Get started
+                    </Button>
                 </div>
             </div>
         </Section>
