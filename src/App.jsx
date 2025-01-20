5882978c1522cceb94047f6f34340b2cb6d7b52c
Giorgia <giorgia.giro@gmail.com>
2025-01-20 15:34:47 +0200

    header setup

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,13 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import ButtonGradient from "./assets/svg/ButtonGradient";
+import Header from "./components/Header";
 
 const App = () => {
     return (
         <>
             <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
-                <h1>sxhecw</h1>
-                <button className="mt-10">Something</button>
+                <Header />
             </div>
             <ButtonGradient />
         </>
