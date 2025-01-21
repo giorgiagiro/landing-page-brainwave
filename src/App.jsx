72507c6a5fb2caa2f2637949c778e57891f0a904
Giorgia <giorgia.giro@gmail.com>
2025-01-21 17:45:06 +0200

    benefits update

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import ButtonGradient from "./assets/svg/ButtonGradient";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
---
+++ src/components/TagLine.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const TagLine = () => {
+    return <div>TagLine</div>;
+};
+
+export default TagLine;
