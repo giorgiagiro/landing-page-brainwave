2cb90eee7c6e1725adf20de228123e3eb37a7c16
Giorgia <giorgia.giro@gmail.com>
2025-01-21 17:18:26 +0200

    benefits, heading

---
+++ src/components/Benefits.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Benefits = () => {
+    return (
+        <section id="features">
+            <div className="container relative z-2">
+                <Heading />
+            </div>
+        </section>
+    );
+};
+
+export default Benefits;
