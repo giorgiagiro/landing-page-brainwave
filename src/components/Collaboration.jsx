a1abe7469fed2c2a3febf4028384d5a0e6259512
Giorgia <giorgia.giro@gmail.com>
2025-01-28 16:06:07 +0200

    update collaboration

--- src/components/Collaboration.jsx
+++ src/components/Collaboration.jsx
@@ -1,27 +1,40 @@
 import React from "react"
 import Section from "./Section"
-import { collabContent } from "../constants"
-import { check } from "../assets"
+import Button from "./Button"
+import { collabContent, collabText } from "../constants"
+import { brainwaveSymbol, check } from "../assets"
 
 const Collaboration = () => {
     return (
         <Section crosses>
             <div className="container lg:flex">
                 <div className="max-w-[25rem]">
-                    <h2 className="h-2 mb-4 md:mb-8">AI CHat App for seamless collaboration</h2>
+                    <h2 className="h-2 mb-4 md:mb-8">AI Chat App for seamless collaboration</h2>
                     <ul className="max-w-[22rem] mb-10 md:mb-14">
                         {collabContent.map((item) => (
                             <li className="mb-3 py-3" key={item.id}>
                                 <div className="flex items-center">
                                     <img src={check} width={24} height={24} alt="check" />
                                     <h6 className="body-2 ml-5">{item.title}</h6>
-                                    {item.title && (
-                                        <p className="body-2 mt-3 text-n-4">{item.text}</p>
-                                    )}
                                 </div>
+                                {item.text && <p className="body-2 mt-3 text-n-4">{item.text}</p>}
                             </li>
                         ))}
                     </ul>
+
+                    <Button>Try it now</Button>
+                </div>
+
+                <div className="ld:ml-auto xl:w-[38rem] mt-4">
+                    <p className="body-2 mb-4 text-n-4 md:mb-16 lg:mb-32 lg:w-[22rem] lg:mx-auto">
+                        {collabText}
+                    </p>
+
+                    <div className="relative left-1/2 flex w-[22rem] aspect-square border border-n-6 rounded-full -translate-x-1/2 scale:75 md:scale-100">
+                        <div className="flex w-60 aspect-square m-auto border border-n-6 rounded-full">
+                            <img src={brainwaveSymbol} width={48} height={48} alt="brainwave" />
+                        </div>
+                    </div>
                 </div>
             </div>
         </Section>
