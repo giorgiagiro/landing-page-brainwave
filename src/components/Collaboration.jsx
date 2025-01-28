ef181cd3a4bf2645e9d27931592fc8e28f6e44bd
Giorgia <giorgia.giro@gmail.com>
2025-01-28 16:21:23 +0200

    syntax fixes

--- src/components/Collaboration.jsx
+++ src/components/Collaboration.jsx
@@ -3,14 +3,15 @@ import Section from "./Section"
 import Button from "./Button"
 import { collabApps, collabContent, collabText } from "../constants"
 import { brainwaveSymbol, check } from "../assets"
-import { Leftcurve, Rightcurve } from "./design/Collaboration"
+import { LeftCurve, RightCurve } from "./design/Collaboration"
 
 const Collaboration = () => {
     return (
         <Section crosses>
             <div className="container lg:flex">
                 <div className="max-w-[25rem]">
-                    <h2 className="h-2 mb-4 md:mb-8">AI Chat App for seamless collaboration</h2>
+                    <h2 className="h2 mb-4 md:mb-8">AI Chat App for seamless collaboration</h2>
+
                     <ul className="max-w-[22rem] mb-10 md:mb-14">
                         {collabContent.map((item) => (
                             <li className="mb-3 py-3" key={item.id}>
@@ -26,7 +27,7 @@ const Collaboration = () => {
                     <Button>Try it now</Button>
                 </div>
 
-                <div className="ld:ml-auto xl:w-[38rem] mt-4">
+                <div className="lg:ml-auto xl:w-[38rem] mt-4">
                     <p className="body-2 mb-8 text-n-4 md:mb-16 lg:mb-32 lg:w-[22rem] lg:mx-auto">
                         {collabText}
                     </p>
@@ -70,8 +71,8 @@ const Collaboration = () => {
                             ))}
                         </ul>
 
-                        <Leftcurve />
-                        <Rightcurve />
+                        <LeftCurve />
+                        <RightCurve />
                     </div>
                 </div>
             </div>
